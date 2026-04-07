6394eb3b4b887e4fb652b387e8e75bfd78d52c8e
Arthur Tavares <felliphe84@gmail.com>
2026-04-07 20:37:18 -0300

    Adicionado exercicio 1

---
+++ exercicios.py
@@ -0,0 +1,7 @@
+#1 Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# num1 = int(input("Digite o primeiro número inteiro: "))
+# num2 = int(input("Digite o segundo número inteiro: "))
+num1 = 8  # Exemplo de entrada
+num2 = 12  # Exemplo de entrada
+resultado_soma = num1 + num2
+print("A soma é:", resultado_soma)
